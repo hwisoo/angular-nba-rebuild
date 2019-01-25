def49635ca5e920ca2131b0f4030aa58c8604d53
James Cho <hwisoocho@gmail.com>
2019-01-25 09:23:06 -0800

    add player model

---
+++ src/app/models/player.model.ts
@@ -0,0 +1,3 @@
+export class Player {
+  constructor(public fname: string, public lname: string, public team: string, public number: number, public smpg: number, public sfgp: number, public stpp: number, public sftp: number, public sppg: number, public srpg: number, public sapg: number, public sbpg: number) { }
+}
